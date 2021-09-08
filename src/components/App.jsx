841e16869b99a200134d16b771ca5c8ecabc8333
Nakkia Roberts <nakkiasr@live.com>
2021-09-08 01:15:53 -0700

    Working on implementing memory and displaying the books in the browser.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,30 @@ import TitleBar from "./TitleBar/TitleBar";
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.book = [
+            {title: "The Left Hand of God", author: "I forget at the moment"},
+            {title: "The Alchemist", author: "Paulo Coelho (I think)"},
+            {title: "The Richest Man in Babylon", author: "Seriously...I forget this too. I'm tired!"}
+        ];
+        this.state = {
+            
+        };
     }
     render() {
         return (
             <div className="container-fluid">
                 <TitleBar />
+                <div className="col-md-4">
+                    {/*Button here to move to the previous book viewed */}
+                    </div>
+                    <div className="col-md-4">
+                    {/*Display book with cover here*/
+                    <h1>{this.books[this.state.bookNumer].title}</h1> 
+    }
+                    </div>   
+                    <div className="col-md-4">
+                    {/*Button here to move to the next book viewed */}
+               </div>
             </div>
         )
     }
