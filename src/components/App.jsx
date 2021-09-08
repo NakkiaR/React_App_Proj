e72d6b0c54bfbc45cb80562eb6ca435986dfaf9b
Nakkia Roberts <nakkiasr@live.com>
2021-09-07 22:42:33 -0700

    Created component, class component, and CSS files. Linked the index files with the app file. Installed Bootstrap and added styling.

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "./App.css"
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <h1>My React App from a Component File</h1>
+        )
+    }
+}
+export default App;
