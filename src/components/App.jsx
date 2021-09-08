22749d05efad533c8dda8cb57fc8edfd31995c45
Nakkia Roberts <nakkiasr@live.com>
2021-09-08 09:41:09 -0700

    Created the buttons and the create book.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,61 @@
 import React, { Component } from 'react';
 import "./App.css"
 import TitleBar from "./TitleBar/TitleBar";
+import BookViewer from './BookViewer/BookViewer';
+import BookCreator from './BookCreator/BookCreator';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.book = [
+        this.books = [
             {title: "The Left Hand of God", author: "I forget at the moment"},
             {title: "The Alchemist", author: "Paulo Coelho (I think)"},
             {title: "The Richest Man in Babylon", author: "Seriously...I forget this too. I'm tired!"}
         ];
         this.state = {
-            
+            bookNumber: 1
         };
     }
+
+    goToNextBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    goToPreviousBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber --;
+        if(tempBookNumber < 0) {
+            tempBookNumber = this.books.length - 1;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    createBook = (newBook) => {
+        console.log('From the createBook on App component', newBook);
+        this.books.push(newBook)
+        this.setState({
+            bookNumber: this.books.length - 1
+        })
+    }
+    
     render() {
         return (
             <div className="container-fluid">
                 <TitleBar />
-                <div className="col-md-4">
-                    {/*Button here to move to the previous book viewed */}
-                    </div>
-                    <div className="col-md-4">
-                    {/*Display book with cover here*/
-                    <h1>{this.books[this.state.bookNumer].title}</h1> 
-    }
-                    </div>   
-                    <div className="col-md-4">
-                    {/*Button here to move to the next book viewed */}
-               </div>
+                <BookViewer book={this.books[this.state.bookNumber]} nextBook={this.goToNextBook} previousBook={this.goToPreviousBook} />
+                <BookCreator createNewBook={this.createBook} />
             </div>
         )
     }
 }
+    
 export default App;
